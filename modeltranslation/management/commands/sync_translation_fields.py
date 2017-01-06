63de45e1323e608e33ca46bfe3bef246ae8ef1fa
Gaël UTARD <gael.utard@makina-corpus.com>
2017-01-06 20:40:39 +0100

    Fix sync_translation_fields command when db_column is set for some fields

--- modeltranslation/management/commands/sync_translation_fields.py
+++ modeltranslation/management/commands/sync_translation_fields.py
@@ -71,7 +71,7 @@ class Command(NoArgsCommand):
             opts = translator.get_options_for_model(model)
             for field_name, fields in opts.local_fields.items():
                 # Take `db_column` attribute into account
-                field = list(fields)[0]
+                field = list(fields)[0].translated_field
                 column_name = field.db_column if field.db_column else field_name
                 missing_langs = list(self.get_missing_languages(column_name, db_table))
                 if missing_langs:
